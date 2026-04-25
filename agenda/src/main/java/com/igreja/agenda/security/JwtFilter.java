15ccbe9de78fda557e63c5ce2ff3d746bb275436
adtcarvalhos-dev <adtcarvalhos.dev@gmail.com>
2026-04-24 22:23:49 -0300

    Update JwtFilter.java

--- agenda/src/main/java/com/igreja/agenda/security/JwtFilter.java
+++ agenda/src/main/java/com/igreja/agenda/security/JwtFilter.java
@@ -8,22 +8,20 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.jspecify.annotations.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
-    private final UserDetailsService userDetailsService;
 
-    public JwtFilter(JwtService jwtService, UserDetailsService userDetailsService) {
+    public JwtFilter(JwtService jwtService) {
         this.jwtService = jwtService;
-        this.userDetailsService = userDetailsService;
     }
 
     @Override
@@ -40,16 +38,20 @@ public class JwtFilter extends OncePerRequestFilter {
 
             try {
                 String email = jwtService.getEmail(token);
+                String role = jwtService.getRole(token); // 🔥 NOVO
 
                 if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 
-                    UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+                    // 🔥 AGORA USA ROLE DO TOKEN
+                    var authorities = List.of(
+                            new SimpleGrantedAuthority("ROLE_" + role)
+                    );
 
                     UsernamePasswordAuthenticationToken authentication =
                             new UsernamePasswordAuthenticationToken(
-                                    userDetails,
+                                    email,
                                     null,
-                                    userDetails.getAuthorities()
+                                    authorities
                             );
 
                     SecurityContextHolder.getContext().setAuthentication(authentication);
